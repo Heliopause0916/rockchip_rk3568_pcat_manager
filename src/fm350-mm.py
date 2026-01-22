c4f0d5083664444fb655980f09933e81fc397b2f
Kyosuke Nekoyashiki <supercatexpert@gmail.com>
2026-01-22 13:05:39 +0900

    fix: remove AT+COPS command result checking on FM350 as the device may not return result sometimes.

--- src/fm350-mm.py
+++ src/fm350-mm.py
@@ -115,11 +115,6 @@ def fm350_at_dial(sr, pdp_index, apn_str):
     sr.send_data("AT+COPS=0,0")
     sbuf = sr.recv_data_with_timeout(timeout=5).strip()
 
-    if not "OK" in sbuf:
-        sr.close()
-        print("AT command AT+COPS failed: {0}".format(sbuf))
-        sys.exit(5)
-
     sr.recv_data_clear()
     sr.send_data('AT+CGDCONT={0},"IP","{1}"'.format(pdp_index, apn_str))
     sbuf = sr.recv_data_with_timeout().strip()
